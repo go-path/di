72927b8e1ba8773ce6d3a9eb30f7feb7b9aa52ad
Alex Rodin <contato@alexrodin.info>
2023-12-03 18:43:57 -0300

    update qualify

--- qualifier.go
+++ qualifier.go
@@ -1,40 +1,19 @@
 package di
 
-import (
-	"reflect"
-)
-
 type defaultQualifier string
 
 type Qualifier[T any, Q any] interface {
 	Value() T
-	getValueType() reflect.Type
-	getQualifierType() reflect.Type
 }
 
 func Qualify[T any, Q any](value T) Qualifier[T, Q] {
-	var q *Q
-	return &qualified[T, Q]{
-		value:         value,
-		valueType:     reflect.TypeOf(value),
-		qualifierType: reflect.TypeOf(q),
-	}
+	return &qualified[T, Q]{value: value}
 }
 
 type qualified[T any, Q any] struct {
-	value         T
-	valueType     reflect.Type
-	qualifierType reflect.Type
+	value T
 }
 
 func (q *qualified[T, Q]) Value() T {
 	return q.value
-}
-
-func (q *qualified[T, Q]) getValueType() reflect.Type {
-	return q.valueType
-}
-
-func (q *qualified[T, Q]) getQualifierType() reflect.Type {
-	return q.qualifierType
 }
